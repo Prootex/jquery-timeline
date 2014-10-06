39878509aa47aa5778ecb12f0377f6737eeeff75
Prootex <prootex@gmail.com>
2014-10-06 13:25:24 +0200

    html structure

--- js/jquery-timeline.js
+++ js/jquery-timeline.js
@@ -1,14 +1,21 @@
 (function($){
 
+	var containerCount = $("#jquery-timeline > .wrapper > .row > .container").length,
+		rowWidth = ((containerCount)*($("#jquery-timeline > .wrapper > .row > .container").width()));
+
+	(containerCount == 1)?rowWidth = "100%":"";
+
+
 	$.fn.jqueryTimeline = function(data) {
 		return this.each(function() {
 			(new $.jqueryTimeline($(this), data));
 		});
 	};
+
 	$.jqueryTimeline = function(element, data) {
-		var structure = "<div class=\"heading\">"+data.timeline.headline+"</div> \
-		<img src=\""+data.timeline.asset.media+"\" alt=\""+data.timeline.asset.caption+"\" />";
-		element.html(structure);
+
+		element.find(".wrapper > .row").css({"width": rowWidth});
+		// element.html(structure);
 	};
 
 })(jQuery);
