f2d5afdf1e81a68a7c32036a6d180c8a080b5489
Prootex <prootex@gmail.com>
2014-10-07 18:12:09 +0200

    navigation move animation

--- js/jquery-timeline.js
+++ js/jquery-timeline.js
@@ -14,9 +14,6 @@
 			animationSpeed = 300,
 			leftPosition = 0;
 
-		/* hide left icon-arrow */
-		// magicButtons();
-
 		/* several changes to the sizes */
 		rowWidth = (containerCount == 1)?"100%":rowWidth + 10000;
 		if(!(element.find(".media").html())) {
@@ -42,7 +39,7 @@
 				sliderTransition = 'all 1500ms cubic-bezier(.71,.08,.35,.87)',
 				clickCount = 0,
 
-				moveTo = function(position) {
+				moveTo = function(position, clickCount) {
 					row.css({
 						'-webkit-transform' : 'translate3d('+(-position)+'px, 0px, 0px)',
 						'-moz-transform' : 'translate3d('+(-position)+'px, 0px, 0px)',
@@ -53,6 +50,7 @@
 						'-o-transition' : sliderTransition,
 						'transition' : sliderTransition
 					});
+					magicButtons(clickCount);
 				},
 
 				onBtnLeft = function() {
@@ -62,7 +60,7 @@
 						isMoving = true;
 						clickCount--;
 
-						moveTo(currentWrapperWidth*clickCount);
+						moveTo(currentWrapperWidth*clickCount, clickCount);
 
 						setTimeout(function() {
 							isMoving = false;
@@ -77,7 +75,7 @@
 						isMoving = true;
 						clickCount++;
 
-						moveTo(currentWrapperWidth*clickCount);
+						moveTo(currentWrapperWidth*clickCount, clickCount);
 
 						setTimeout(function() {
 							isMoving = false;
@@ -90,40 +88,72 @@
 						btnRight = element.find("#right");
 
 					currentWrapperWidth = element.find(".wrapper").width();
-
 					eventCount = container.size();
 					if (eventCount > 1) {
 						btnLeft
-							.show()
 							.bind("click", onBtnLeft);
 						btnRight
-							.show()
 							.bind("click", onBtnRight);
 					}
 				},
 
 				onLoad = function() {
+					/* hide left button */
+					element.find("#left").hide();
+
 					init();
 				};
 			onLoad();
 		};
 
-		/* hide show buttons */
-		magicButtons = function () {
-			if (element.find(".row").css("left") == "0px") {
-				element.find("#left").hide();
+		/* hide show animation for prev/next buttons */
+		magicButtons = function (clickCount) {
+			if (clickCount == 0) {
+				element.find("#left").animate({opacity:"hide"}, 500);
 			}
 			else {
-				element.find("#left").show();
-				if (element.find(".row").css("left") == -(rowWidth-wrapperWidth)) {
-					element.find("#right").hide();
+				element.find("#left").animate({opacity:"show"}, 1500);
+				if (clickCount == (containerCount-1)) {
+					element.find("#right").animate({opacity:"hide"}, 500);
 				}
 				else {
-					element.find("#right").show();
+					element.find("#right").animate({opacity:"show"}, 1500);
 				}
 			}
 		}
+
 		initEventAnimation();
+
+		/* Timeline navigation */
+		var clicked = false,
+			mousePosition = 0,
+			left = 0,
+			position = 0;
+
+		element.find("#navigation")
+			.mousemove(function( event ) {
+				if (!clicked) return;
+
+				var pageCoords = parseInt(event.pageX);
+
+				if (pageCoords < mousePosition) {
+					left = left - parseInt(position + (mousePosition-pageCoords));
+				}
+				else {
+					left = parseInt(position - (pageCoords-mousePosition));
+				}
+
+				element.find("#navigation .navigation-row").css("left", -left);
+			});
+
+		element.find("#navigation")
+			.mousedown(function() {
+				clicked = true;
+				mousePosition = window.event.clientX;
+			})
+			.mouseup(function() {
+				clicked = false;
+			});
 	};
 
 })(jQuery);
